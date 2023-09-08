68e7703ecf22ecf187efaf7f857210ff5af90a93
vepetrelli <vepetrelli@gmail.com>
2023-09-08 10:30:19 -0300

    Update PortfolioProyect1.sql

--- PortfolioProyect1.sql
+++ PortfolioProyect1.sql
@@ -33,9 +33,42 @@ WHERE continent IS NOT NULL
 GROUP BY location
 ORDER BY TotalDeathsCount desc
 
---Grouped by Continent
+--Continents with the highest death count per population
 SELECT continent, MAX(total_deaths) AS TotalDeathsCount
 FROM PortfolioProyect1..['CovidDeaths']
+WHERE continent is not null
 GROUP BY continent
 ORDER BY TotalDeathsCount desc
 
+--Global numbers by date
+SELECT date, SUM(new_cases) as total_cases, SUM(new_deaths) as total_deaths, SUM(new_deaths)/nullif(SUM(new_cases),0)*100 as DeathPercentage
+FROM PortfolioProyect1..['CovidDeaths']
+WHERE continent is not null
+GROUP BY date
+ORDER BY 1,2
+
+--Total global numbers
+SELECT SUM(new_cases) as total_cases, SUM(new_deaths) as total_deaths, SUM(new_deaths)/nullif(SUM(new_cases),0)*100 as DeathPercentage
+FROM PortfolioProyect1..['CovidDeaths']
+WHERE continent is not null
+ORDER BY 1,2
+
+--Vaccinations
+Select * FROM PortfolioProyect1..['CovidVaccinations']
+
+--Tables join
+Select * FROM PortfolioProyect1..['CovidVaccinations'] vac
+JOIN PortfolioProyect1..['CovidDeaths'] dea
+ON dea.location=vac.location
+AND dea.date=vac.date
+
+--Total vaccinations
+Select dea.continent, dea.location, dea.date, dea.population, vac.new_vaccinations
+FROM PortfolioProyect1..['CovidVaccinations'] vac
+JOIN PortfolioProyect1..['CovidDeaths'] dea
+ON dea.location=vac.location
+AND dea.date=vac.date
+where dea.continent is not null
+ORDER BY 1,2,3
+
+https://www.youtube.com/watch?v=qfyynHBFOsM&t=2285s&ab_channel=AlexTheAnalyst 56:22
